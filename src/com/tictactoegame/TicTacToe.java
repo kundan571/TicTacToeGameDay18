9a8f113127116c72b7d4d1b99714f27c00ab773a
kundan571 <kundanchauhan1997@gmail.com>
2023-03-03 20:03:35 +0530

    Added: Display TicTacToe Board for play game

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -2,7 +2,36 @@ package com.tictactoegame;
 
 public class TicTacToe {
 
+    char[][] board;
+
+    public TicTacToe() {
+        this.board = new char[3][3];
+        initialiseBoard();
+    }
+
+    void initialiseBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    void displayBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < board.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe Game:!!");
+        TicTacToe tacToe = new TicTacToe();
+        tacToe.displayBoard();
     }
 }
